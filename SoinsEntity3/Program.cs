5c642ff361f062bb8a989f7fa7e3cedf7d08a432
RAHMANE Amine <rahmane.a@sio.lan>
2016-10-05 15:02:28 +0200

    scripts

--- SoinsEntity3/Program.cs
+++ SoinsEntity3/Program.cs
@@ -11,16 +11,6 @@ namespace SoinsEntity3
     {
         static void Main(string[] args)
         {
-            OracleConnection con = new OracleConnection();
-            
-            con.ConnectionString = "SERVER=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=10.10.2.10)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=slam)));User id=rahmanesoin; Password=slam;";
-            con.Open();
-            Console.WriteLine("Connected to Oracle" + con.ServerVersion);
-
-            // Close and Dispose OracleConnection object
-            con.Close();
-            con.Dispose();
-            Console.WriteLine("Disconnected");
             Console.ReadLine();
         }
     }
